2e40247f8ca08c574f28e1a1e0c110a756a14f77
Hahahayabanan <oleg.volobuev@fullstack-development.com>
2020-08-11 15:23:49 +0400

    feat: add label, comingsoon, infoiconv2

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -5,3 +5,4 @@ export { Button, ButtonProps } from './Button/Button';
 export * from './Tabs/Tabs';
 export * from './Metric/Metric';
 export * from './ComingSoon/ComingSoon';
+export * from './Label/Label';
