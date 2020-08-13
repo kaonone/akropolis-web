5710212c3433bb6fe6b295f2a1b39b2c313316f3
imin314 <imin314@gmail.com>
2020-08-13 15:07:31 +0700

    fix: export RadioButton

--- packages/components/src/inputs/index.ts
+++ packages/components/src/inputs/index.ts
@@ -7,5 +7,6 @@ export { NumberInput } from './NumberInput';
 export { DecimalsInput } from './DecimalsInput';
 export { CheckboxInput } from './CheckboxInput';
 export { RadioGroupInput } from './RadioGroupInput';
+export { RadioButton } from './RadioButton/RadioButton';
 export { SwitchInput } from './SwitchInput/SwitchInput';
 export { SelectInput } from './SelectInput/SelectInput';
