7066d530178a52e16b0a9d1dded02f7260e3ab6a
imin314 <imin314@gmail.com>
2020-08-19 19:53:49 +0700

    feat: extend Button and Metric components
    Style textPrimary variant of Button, rename titleSize Metric prop to size

--- packages/components/src/Metric/Metric.style.ts
+++ packages/components/src/Metric/Metric.style.ts
@@ -1,29 +1,38 @@
-import { makeStyles, Theme, colors } from '@akropolis-web/styles';
+import { makeStyles } from '@akropolis-web/styles';
 
-export const useStyles = makeStyles((theme: Theme) => ({
-  root: {},
-
-  title: {
-    lineHeight: `${theme.spacing(3.5)}px`,
-    textTransform: 'uppercase',
-    opacity: 0.8,
-  },
-
-  values: {
-    whiteSpace: 'nowrap',
-  },
-
-  value: {
-    display: 'inline-block',
-    verticalAlign: 'baseline',
-    lineHeight: `${theme.spacing(4)}px`,
-    fontWeight: 500,
-  },
-
-  subValue: {
-    display: 'inline-block',
-    verticalAlign: 'baseline',
-    marginLeft: theme.spacing(1),
-    color: colors.frenchGray,
-  },
-}));
+export const useStyles = makeStyles(
+  () => ({
+    root: {},
+    title: {
+      fontSize: 16,
+      marginBottom: 12,
+    },
+    sizeSmall: {
+      '& $title': {
+        fontSize: 12,
+        fontWeight: 300,
+        marginBottom: 0,
+      },
+      '& $value': {
+        fontSize: 22,
+      },
+    },
+    value: {
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: 32,
+      fontWeight: 300,
+      lineHeight: 'normal',
+    },
+    subValue: {
+      marginTop: 8,
+      fontSize: 16,
+    },
+    chart: {
+      marginBottom: 5,
+      marginLeft: 10,
+      alignSelf: 'flex-end',
+    },
+  }),
+  { name: 'Metric' },
+);
--- packages/components/src/Metric/Metric.tsx
+++ packages/components/src/Metric/Metric.tsx
@@ -1,24 +1,23 @@
 import * as React from 'react';
 import cn from 'classnames';
-import { makeStyles } from '@akropolis-web/styles';
+
+import { useStyles } from './Metric.style';
 
 type Props = {
   title: NonNullable<React.ReactNode>;
   value: NonNullable<React.ReactNode>;
   subValue?: React.ReactNode;
   chart?: React.ReactNode;
-  titleSize?: 'small' | 'medium';
+  size?: 'small' | 'medium';
 };
 
 export function Metric(props: Props) {
-  const { title, value, subValue, chart, titleSize = 'medium' } = props;
+  const { title, value, subValue, chart, size = 'medium' } = props;
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={cn(classes.title, { [classes.titleSizeSmall]: titleSize === 'small' })}>
-        {title}
-      </div>
+    <div className={cn(classes.root, { [classes.sizeSmall]: size === 'small' })}>
+      <div className={classes.title}>{title}</div>
       <div className={classes.value}>
         {value}
         {chart && <div className={classes.chart}>{chart}</div>}
@@ -27,35 +26,3 @@ export function Metric(props: Props) {
     </div>
   );
 }
-
-const useStyles = makeStyles(
-  () => ({
-    root: {},
-    title: {
-      fontSize: 16,
-      marginBottom: 12,
-    },
-    titleSizeSmall: {
-      fontSize: 12,
-      fontWeight: 300,
-      marginBottom: 2,
-    },
-    value: {
-      display: 'flex',
-      alignItems: 'center',
-      fontSize: 32,
-      fontWeight: 300,
-      lineHeight: 'normal',
-    },
-    subValue: {
-      marginTop: 8,
-      fontSize: 16,
-    },
-    chart: {
-      marginBottom: 5,
-      marginLeft: 10,
-      alignSelf: 'flex-end',
-    },
-  }),
-  { name: 'Metric' },
-);
