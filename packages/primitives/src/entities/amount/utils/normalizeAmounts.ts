1040476050f43727ec0f6199ce9b2f705dc27b7f
Geksanit <alserpet@e-mail.ru>
2020-08-31 20:42:29 +0700

    feat(normalizeamounts): add decimals arg for normalizeAmounts

--- packages/primitives/src/entities/amount/utils/normalizeAmounts.ts
+++ packages/primitives/src/entities/amount/utils/normalizeAmounts.ts
@@ -1,18 +1,30 @@
 import { decimalsToWei } from '../../../bnHexWei';
 import { Amount, NormalizedAmount } from '../Amount';
 
-export function normalizeAmounts<T extends Amount>(amounts: T[]): NormalizedAmount<T>[] {
+export function normalizeAmounts<T extends Amount>(
+  amounts: T[],
+  decimals?: number,
+): NormalizedAmount<T>[] {
   if (amounts.length < 1) {
     return [];
   }
 
-  const maxDecimal = amounts
-    .map(amount => amount.currency.decimals)
-    .reduce((max, current) => (current > max ? current : max));
+  const maxDecimal =
+    decimals !== undefined
+      ? decimals
+      : amounts
+          .map(amount => amount.currency.decimals)
+          .reduce((max, current) => (current > max ? current : max));
 
-  return amounts.map(amount => ({
-    decimals: maxDecimal,
-    original: amount,
-    value: amount.toFraction().mul(decimalsToWei(maxDecimal - amount.currency.decimals)),
-  }));
+  return amounts.map(amount => {
+    const diff = maxDecimal - amount.currency.decimals;
+    return {
+      decimals: maxDecimal,
+      original: amount,
+      value:
+        diff > 0
+          ? amount.toFraction().mul(decimalsToWei(diff))
+          : amount.toFraction().div(decimalsToWei(diff)),
+    };
+  });
 }
