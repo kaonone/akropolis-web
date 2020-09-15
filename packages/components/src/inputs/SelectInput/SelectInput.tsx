f0c5fdc70861a25c66f4e94a4505124e475f0ad3
feinminen <feinminen@gmail.com>
2020-09-15 12:36:34 +0700

    fix(selectinput): fix select styles with a lack of space below or higher

--- packages/components/src/inputs/SelectInput/SelectInput.tsx
+++ packages/components/src/inputs/SelectInput/SelectInput.tsx
@@ -1,4 +1,11 @@
-import React, { ComponentPropsWithoutRef, useState, useCallback, useMemo } from 'react';
+import React, {
+  ComponentPropsWithoutRef,
+  useState,
+  useCallback,
+  useMemo,
+  useRef,
+  useEffect,
+} from 'react';
 import cn from 'classnames';
 import MenuItem from '@material-ui/core/MenuItem';
 import { MenuProps } from '@material-ui/core/Menu';
@@ -19,16 +26,22 @@ type OwnProps = {
 
 type SelectInputProps = OwnProps & ComponentPropsWithoutRef<typeof TextInput>;
 
+const MENU_PADDINGS_HEIGHT = 15;
+const MENU_SHIFT_HEIGHT = 20;
+
 export function SelectInput(props: SelectInputProps) {
   const { options, InputProps = {}, ...restProps } = props;
   const { className: inputClassName, ...restInputProps } = InputProps;
   const classes = useStyles();
   const backgroundColor = useAncestorBackgroundHack();
-
+  const currentWindowHeight = useWindowHeight();
   const hasSingleOption = options.length <= 1;
 
   const [isMenuOpen, setIsOpen] = useState(false);
+  const [toBottomDistance, setToBottomDistance] = useState(0);
+  const [toTopDistance, setToTopDistance] = useState(0);
 
+  const selectInputRef = useRef<HTMLDivElement>(null);
   const handleSelectOpen = useCallback(() => {
     setIsOpen(true);
   }, []);
@@ -37,25 +50,42 @@ export function SelectInput(props: SelectInputProps) {
     setIsOpen(false);
   }, []);
 
+  useEffect(() => {
+    setToBottomDistance(
+      currentWindowHeight -
+        Number(selectInputRef.current?.getBoundingClientRect().bottom) -
+        MENU_SHIFT_HEIGHT,
+    );
+    setToTopDistance(
+      Number(selectInputRef.current?.getBoundingClientRect().top) - MENU_SHIFT_HEIGHT,
+    );
+  }, [setToBottomDistance, currentWindowHeight, selectInputRef]);
+  const selectHeight =
+    Number(selectInputRef.current?.offsetHeight) * options.length + MENU_PADDINGS_HEIGHT;
+
+  const hasBottomSpace = toBottomDistance > selectHeight;
+  const hasTopSpace = toTopDistance > selectHeight;
+
   const menuPositionProps = useMemo<Partial<MenuProps>>(
     () => ({
       elevation: 0,
       anchorOrigin: {
-        vertical: 'bottom',
+        vertical: getVerticalPosition('anchor'),
         horizontal: 'center',
       },
       transformOrigin: {
-        vertical: 'top',
+        vertical: getVerticalPosition('transform'),
         horizontal: 'center',
       },
       getContentAnchorEl: null,
     }),
-    [],
+    [toBottomDistance, toTopDistance, selectHeight],
   );
 
   return (
     <TextInput
       {...restProps}
+      ref={selectInputRef}
       select
       variant="outlined"
       disabled={hasSingleOption}
@@ -67,6 +97,12 @@ export function SelectInput(props: SelectInputProps) {
         {
           [classes.withSingleOption]: hasSingleOption,
         },
+        {
+          [classes.hasBottomSpace]: hasBottomSpace,
+        },
+        {
+          [classes.hasTopSpace]: !hasBottomSpace && hasTopSpace,
+        },
       )}
       InputProps={{
         ...restInputProps,
@@ -76,7 +112,15 @@ export function SelectInput(props: SelectInputProps) {
         MenuProps: {
           PaperProps: {
             variant: 'outlined',
-            className: classes.paper,
+            className: cn(
+              classes.paper,
+              {
+                [classes.hasBottomSpace]: hasBottomSpace,
+              },
+              {
+                [classes.hasTopSpace]: !hasBottomSpace && hasTopSpace,
+              },
+            ),
             style: { backgroundColor },
           },
           ...menuPositionProps,
@@ -102,5 +146,32 @@ export function SelectInput(props: SelectInputProps) {
         <Arrow fontSize="inherit" />
       </div>
     );
+  }
+
+  function getVerticalPosition(type: 'anchor' | 'transform') {
+    if (hasBottomSpace) {
+      return type === 'anchor' ? 'bottom' : 'top';
+    }
+    if (hasTopSpace) {
+      return type === 'anchor' ? 'top' : 'bottom';
+    }
+
+    return 'center';
+  }
+
+  function useWindowHeight() {
+    const [windowHeight, setWindowHeight] = useState<number>(0);
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowHeight(window.innerHeight);
+      }
+
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowHeight;
   }
 }
