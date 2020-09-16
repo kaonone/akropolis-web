054bb1fadcfdcebf70ab7a2baf041bd7c64a76a8
Geksanit <alserpet@e-mail.ru>
2020-09-16 21:44:28 +0700

    fix(button): remove gradien from text

--- packages/components/src/Button/Button.style.ts
+++ packages/components/src/Button/Button.style.ts
@@ -174,17 +174,10 @@ export const useStyles = makeStyles(theme => ({
     minWidth: 'unset',
     minHeight: 'unset',
     justifyContent: 'flex-start',
-    background: theme.gradients.outlinedButton.linear('to right'),
-    backgroundSize: '150%',
-    '-webkit-text-fill-color': 'transparent',
-    '-webkit-background-clip': 'text',
-    backgroundClip: 'text',
     borderRadius: 0,
-    transition: 'background-size 1s',
 
     '&:hover, &$focusVisible': {
-      backgroundColor: 'transparent',
-      backgroundSize: '500%',
+      color: '#EB67FE',
     },
   },
 
