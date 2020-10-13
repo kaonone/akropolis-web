fc308f0d8421eae6f69762d4b06385bfa3bb1f57
feinminen <feinminen@gmail.com>
2020-10-13 19:40:42 +0700

    feat(liquidityamount): expand liquidity amount class

--- packages/primitives/src/entities/LiquidityAmount.ts
+++ packages/primitives/src/entities/LiquidityAmount.ts
@@ -3,23 +3,35 @@ import { Value } from '../fraction';
 import { Amount } from './amount';
 import { Currency } from './Currency';
 
+type SymbolPosition = 'start-space' | 'start' | 'end-space' | 'end';
 const uniqType = Symbol('LiquidityAmount type');
 
 export class LiquidityAmount extends Amount<Currency> {
   public _type: typeof uniqType = uniqType;
+
+  private readonly defaultPrecision: number = this.options?.precisions || 2;
+  private readonly defaultSymbolPosition: SymbolPosition = this.options?.symbolPosition || 'start';
+
+  constructor(
+    amount: Value,
+    currency: Currency,
+    private options?: { precisions?: number; symbolPosition?: SymbolPosition },
+  ) {
+    super(amount, currency);
+  }
 
   // eslint-disable-next-line class-methods-use-this
   public makeAmount(amount: Value, currency: Currency): this {
     return new LiquidityAmount(amount, currency) as this;
   }
 
-  public toFormattedString(precision: number = 2, withSymbol = true): string {
+  public toFormattedString(precision: number = this.defaultPrecision, withSymbol = true): string {
     return formatBalance({
       amountInBaseUnits: this.toBN(),
       tokenSymbol: withSymbol ? this.currency.symbol : undefined,
       baseDecimals: this.currency.decimals,
       precision,
-      symbolPosition: 'start',
+      symbolPosition: this.defaultSymbolPosition,
     });
   }
 
@@ -28,7 +40,7 @@ export class LiquidityAmount extends Amount<Currency> {
       amountInBaseUnits: this.toBN(),
       tokenSymbol: withSymbol ? this.currency.symbol : undefined,
       baseDecimals: this.currency.decimals,
-      symbolPosition: 'start',
+      symbolPosition: this.defaultSymbolPosition,
     });
   }
 }
