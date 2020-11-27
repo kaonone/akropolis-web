0d4de45e2b2c39888ddc25b747f347dee18d8ccb
Hahahayabanan <olegvolobuev97@mail.ru>
2020-11-27 18:59:46 +0400

    feat: hide select in amount input, add icon size prop in TokenName

--- packages/components/src/TokenName/TokenName.tsx
+++ packages/components/src/TokenName/TokenName.tsx
@@ -7,27 +7,50 @@ import { SvgIconProps } from '@material-ui/core/SvgIcon';
 
 import { TokenIcon } from '../TokenIcon/TokenIcon';
 
+type IconSizes = 'small' | 'inherit' | 'default' | 'large';
+
 type Props = {
   token: Token;
-  iconProps?: SvgIconProps;
+  iconSize?: IconSizes;
+  iconProps?: Omit<SvgIconProps, 'fontSize'>;
 };
 
-export function TokenName({ token, iconProps = {} }: Props) {
+export function TokenName({ token, iconSize = 'default', iconProps = {} }: Props) {
   const classes = useStyles();
   const { className, ...rest } = iconProps;
 
   return (
     <Grid container alignItems="center" wrap="nowrap">
-      <TokenIcon tokenAddress={token.address} className={cn(className, classes.icon)} {...rest} />
+      <TokenIcon
+        tokenAddress={token.address}
+        className={cn(className, classes.icon, getIconSizeClass(iconSize))}
+        {...rest}
+      />
       {token.symbol}
     </Grid>
   );
+
+  function getIconSizeClass(size?: IconSizes) {
+    return size && classes[size];
+  }
 }
 
 const useStyles = makeStyles(
   {
     icon: {
       marginRight: 8,
+    },
+    default: {
+      fontSize: 24,
+    },
+    small: {
+      fontSize: 20,
+    },
+    large: {
+      fontSize: 30,
+    },
+    inherit: {
+      fontSize: 'inherit',
     },
   },
   { name: 'TokenName' },
