c115c284e95224616f36702dc109ca2b18cd0a18
Hahahayabanan <olegvolobuev97@mail.ru>
2021-10-01 11:29:50 +0300

    feat: [DD-2817] Add border radius prop to skeleton

--- packages/components/src/Skeleton/Skeleton.tsx
+++ packages/components/src/Skeleton/Skeleton.tsx
@@ -9,16 +9,24 @@ import {
   lighten,
 } from '@akropolis-web/styles';
 
+const DEFAULT_BORDER_RADIUS = 6;
+
+type WithRoundedBorders = {
+  borderRadius?: number | 'default';
+};
+
+type StylesProps = ProvidedAncestorBackground & WithRoundedBorders;
+
 export const Skeleton: OverridableComponent<SkeletonTypeMap> = <
   P extends Record<string, unknown>,
   D extends React.ElementType = 'span'
 >(
-  props: SkeletonProps<D, P>,
+  props: SkeletonProps<D, P> & WithRoundedBorders,
 ) => {
-  const { classes: muiClasses = {}, ...rest } = props;
+  const { classes: muiClasses = {}, borderRadius, ...rest } = props;
 
   const backgroundColor = useAncestorBackgroundHack();
-  const classes = useStyles({ backgroundColor });
+  const classes = useStyles({ backgroundColor, borderRadius });
 
   return (
     <MuiSkeleton classes={{ ...muiClasses, root: cn(classes.root, muiClasses.root) }} {...rest} />
@@ -28,11 +36,22 @@ export const Skeleton: OverridableComponent<SkeletonTypeMap> = <
 const useStyles = makeStyles(
   () => ({
     root: {
-      backgroundColor: ({ backgroundColor }: ProvidedAncestorBackground) =>
-        lighten(backgroundColor, 0.15),
+      backgroundColor: ({ backgroundColor }: StylesProps) => lighten(backgroundColor, 0.15),
+      borderRadius: ({ borderRadius }: StylesProps) => getBorderRadius(borderRadius),
     },
   }),
   { name: 'Skeleton' },
 );
+
+function getBorderRadius(borderRadius: StylesProps['borderRadius']) {
+  switch (typeof borderRadius) {
+    case 'undefined':
+      return 'unset';
+    case 'string':
+      return DEFAULT_BORDER_RADIUS;
+    case 'number':
+      return `${borderRadius}px`;
+  }
+}
 
 export { SkeletonProps };
